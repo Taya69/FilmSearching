e7bbba475a5f20ce1e1d08809024ea49f73edf0c
Taya69 <69574818+Taya69@users.noreply.github.com>
2021-05-02 10:41:19 +0300

    Add project files

---
+++ src/components/SearchPhrase.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+     <h2>You searched for: {{searchText}}, {{result}} results found</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+ name: 'SearchPhrase',
+ data () {
+   return {
+    // searchText: localStorage.getItem('searchText')
+   }
+ },
+ props: ['searchText'],
+ computed: {
+ //  searchText () {return localStorage.getItem('searchText')},
+   result () {return this.$store.getters.getResult}
+ }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  margin-left: 3%;
+}
+ 
+</style>
